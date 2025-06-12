eda08a7a84b48f6753d31949cb8c0ab34423c2db
MAC <mac@MACs-MacBook-Pro.local>
2025-06-12 22:30:19 +0700

    fix: remove root from vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  root: 'frontend',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
-  },
-  build: {
-    outDir: 'dist',
   },
 });
